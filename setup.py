775f2614ddfc6db7236b6a48c0028c6897c41f5d
Ludovico Bianchi <lbianchi@lbl.gov>
2021-07-22 13:52:46 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='waml',
+    version="0.1.0dev",
+    description="WAML modeling library",
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords="water systems, chemical engineering, process modeling, filtration, desalination, nawi",
+    python_requires=">=3.6, <4",
+)
